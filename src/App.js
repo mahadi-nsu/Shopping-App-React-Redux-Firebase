194cf0cfe20dd558e74f8011c26534b557fb8c59
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-06-02 15:02:54 +0600

    homepage jsx and styles added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
+
 function App() {
   return (
-    <div className="App">
-      Mahadi
+    <div>
+      <HomePage />
     </div>
   );
 }
