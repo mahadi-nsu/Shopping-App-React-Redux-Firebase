b186ba618216c69e14c6710f075ba15445964503
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-06-06 13:54:35 +0600

    some tricky css added for background image

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrls, size }) => (
-    <div className="menu-item">
-        <div className="content">
-            <div className="title"> {title.toUpperCase()} </div>
-            <span className="subtitle">Shop Now</span>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div
+        className={`${size} menu-item`}
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+    >
+        <div>
+            <div className="content">
+                <div className="title"> {title.toUpperCase()} </div>
+                <span className="subtitle">Shop Now</span>
+            </div>
         </div>
     </div>
 )
