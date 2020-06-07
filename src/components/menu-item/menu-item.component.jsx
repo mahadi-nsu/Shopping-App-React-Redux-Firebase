11b0eb76dcec80a7a37eaf7bfaf627986f66a67c
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-06-07 23:41:23 +0600

    transition added

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,19 +3,18 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div
-        className={`${size} menu-item`}
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-    >
-        <div>
-            <div className="content">
-                <div className="title"> {title.toUpperCase()} </div>
-                <span className="subtitle">Shop Now</span>
-            </div>
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-)
+);
 
 export default MenuItem;
