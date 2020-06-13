8f5047e54ea0bfb282a4cc0631ffaf30170779b1
Mahadi Hassan <mahadi.hassan@northsouth.edu>
2020-06-13 15:49:48 +0600

    introdduced routing to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+// import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+
+const HomePage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        Home Page
+      </h1>
+    </div>
+  )
+}
+
+const TopicList = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        Topic List  Page
+      </h1>
+    </div>
+  )
+}
+
+const TopicDetails = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        Topic Details  Page
+      </h1>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicList} />
+      <Route path='/topics/:topicId' component={TopicDetails} />
     </div>
   );
 }
